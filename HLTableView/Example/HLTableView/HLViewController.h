6c176b63b16869f29f7a64920864a69c24c40108
SeaDragon <771145867@qq.com>
2018-08-04 18:30:38 +0800

    second commit

---
+++ HLTableView/Example/HLTableView/HLViewController.h
@@ -0,0 +1,13 @@
+//
+//  HLViewController.h
+//  HLTableView
+//
+//  Created by 771145867@qq.com on 04/11/2018.
+//  Copyright (c) 2018 771145867@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HLViewController : UIViewController
+
+@end
---
+++ HLTableView/Example/HLTableView/HLViewController.m
@@ -0,0 +1,29 @@
+//
+//  HLViewController.m
+//  HLTableView
+//
+//  Created by 771145867@qq.com on 04/11/2018.
+//  Copyright (c) 2018 771145867@qq.com. All rights reserved.
+//
+
+#import "HLViewController.h"
+
+@interface HLViewController ()
+
+@end
+
+@implementation HLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
